17e8cd51eb6b439b3d018d99f5fb2b15fc304043
Emanuele Fontana <fontanaemanuele14@gmail.com>
2024-03-26 16:54:56 +0100

    Completamento

--- Interpolazione/MatLab/pol.m
+++ Interpolazione/MatLab/pol.m
@@ -1,1 +1,21 @@
-pol=@(t) coef(1)+coef(2).*(t-x(1))+coef(3).*(t-x(1)).*(t-x(2))+coef(4).*(t-x(1)).*(t-x(2)).*(t-x(3));
+function polynomial = pol(coef,x)
+    % Controllo che la lunghezza di coef e x sia coerente
+    assert(length(coef) == length(x), 'Il numero di coefficienti deve essere uguale al numero di elementi in x.');
+
+    % Numero di coefficienti
+    n = length(coef);
+
+    % Inizializza la stringa della funzione polinomiale
+    poly_string = sprintf('%f', coef(1));
+
+    % Aggiungi i termini del polinomio
+    for i = 2:n
+        poly_string = strcat(poly_string, sprintf(' + %f', coef(i)));
+        for j = 1:i-1
+            poly_string = strcat(poly_string, sprintf(' .* (t - %f)', x(j)));
+        end
+    end
+
+    % Valuta la stringa come una funzione anonima
+    polynomial = str2func(['@(t)' poly_string]);
+end
