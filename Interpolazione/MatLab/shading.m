c95a3ba1a105cf9fc75a30b8b767bec78a3b1984
Emanuele Fontana <fontanaemanuele14@gmail.com>
2024-05-14 19:40:41 +0200

    Shading

--- Interpolazione/MatLab/shading.m
+++ Interpolazione/MatLab/shading.m
@@ -9,39 +9,36 @@ v4=[194,18,89];
 %Effettuiamo l'interpolazione bilineare tra i colori dei vertici per ottenere poi l'effetto di shading
 
 %creazione di una matrice di 100x100x3 per contenere i colori
-img=zeros(100,100,3);
-img(1,1,:)=v1;
-img(1,100,:)=v2;
-img(100,1,:)=v3;
-img(100,100,:)=v4;
-for i=1:100
-    for j=1:100
+%img=zeros(100,100,3);
+%img(1,1,:)=v1;
+%img(1,100,:)=v2;
+%img(100,1,:)=v3;
+%img(100,100,:)=v4;
+%for i=1:100
+ %   for j=1:100
         %f(xi,y0)
-        a=img(1,1,:).*(i-100)/(1-100)+img(100,1,:).*(i-1)/(100-1);
-        a
+        %a=img(1,1,:).*(i-100)/(1-100)+img(100,1,:).*(i-1)/(100-1);
         %f(xi,y1)
-        b=img(1,100,:).*(i-100)/(1-100)+img(100,100,:).*(i-1)/(100-1);
+        %b=img(1,100,:).*(i-100)/(1-100)+img(100,100,:).*(i-1)/(100-1);
         %f(xi,yi)
-        img(i,j,:)=a.*(j-100)/(1-100)+b.*(j-1)/(100-1);
-    end
-end
-imshow(uint8(img));
+        %img(i,j,:)=a.*(j-100)/(1-100)+b.*(j-1)/(100-1);
+  %  end
+%end
+%imshow(uint8(img));
 
 
 
 %Operazioni vettoriali (1 for)
 
 
-%img2=zeros(100,100,3);
-%img2(1,1,:)=v1;
-%img2(1,100,:)=v2;
-%img2(100,1,:)=v3;
-%img2(100,100,:)=v4;
-%for i=1:100
- %   a=img(1,1,:).*(i-100)/(1-100)+img(100,1,:).*(i-1)/(100-1);
-        %f(xi,y1)
-  %  b=img(1,100,:).*(i-100)/(1-100)+img(100,100,:).*(i-1)/(100-1);
-   % img2(i,:,:)=(a.*(1:100-100)/(1-100))'+(b.*(1:100-1)/(100-1))';
-%end
-
-%imshow(uint8(img2));
+img=zeros(100,100,3);
+img(1,1,:)=v1;
+img(1,100,:)=v2;
+img(100,1,:)=v3;
+img(100,100,:)=v4;
+a = img(1, 1, :) .* ((1:100) -100)/(1-100) + img(100, 1, :) .* ((1:100) - 1)/(100-1);
+b = img(1, 100, :) .* ((1:100) - 100)/(1-100) + img(100, 100, :) .* ((1:100) - 1)/(100-1);
+for i=1:100
+    img(i, :, :) = a .* (i-100)/(1-100) + b .* (i-1)/(100-1);
+end
+imshow(uint8(img));
