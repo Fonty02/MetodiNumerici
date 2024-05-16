a546854c33124a204e52f3743b937a3d0a3f6c26
Fonty02 <fontanaemanuele14@gmail.com>
2024-05-16 14:45:47 +0200

    SVD random

--- SVD/Python/randomizedSVD.py
+++ SVD/Python/randomizedSVD.py
@@ -7,35 +7,59 @@ def randomizedSVD(A,l):
     Q, _ = np.linalg.qr(Y, mode='complete')
     B = Q @ A
     Sigma=np.zeros((m,n))
-    U_tilde, S, V = np.linalg.svd(B, full_matrices=True, compute_uv=True)
+    U_tilde, S, V = np.linalg.svd(B, full_matrices=True, compute_uv=True) #viene già restituita V trasposta
     U = Q @ U_tilde
     Sigma[:m,:m] = np.diag(S)
     return U, Sigma, V
 
 def randomizedSVD_v2(A,l,q):
+    m, n = A.shape
+    Omega = np.random.randn(n, l)
+    Y0 = A @ Omega
+    Q0, R0 = np.linalg.qr(Y0, mode='complete')
+    
+    for i in range(1, q):
+        Yi = A.T @ Q0
+        Qi, Ri = np.linalg.qr(Yi, mode='complete')
+        Y0 = A @ Qi
+        Q0, R0 = np.linalg.qr(Y0, mode='complete')
+    
+    B = Q0 @ A  
+    Sigma = np.zeros((m, n))
+    U_tilde, S, V = np.linalg.svd(B, full_matrices=True, compute_uv=True)
+    U = Q0 @ U_tilde
+    Sigma[:m, :m] = np.diag(S) 
+    
+    return U, Sigma, V
+
+
+def randomizedSVD_v3(A,l,q):
    m,n = A.shape
    Omega = np.random.randn(n,l)
-   Y_0=A @ Omega
-   Q0,R0 = np.linalg.qr(Y_0, mode='complete')
+   prodotto=A.T@A
    for i in range(1,q):
-    Yi=A.T @ Q0
-    Qi,Ri = np.linalg.qr(Yi, mode='complete')
-    Y0=A @ Qi
-    Q0,R0 = np.linalg.qr(Y0, mode='complete')
-   B = Q0 @ A
+    prodotto=prodotto@prodotto
+   Y=A @ Omega
+   Q,_ = np.linalg.qr(Y, mode='complete')
+   B = Q @ A
    Sigma=np.zeros((m,n))
    U_tilde, S, V = np.linalg.svd(B, full_matrices=True, compute_uv=True)
-   U = Q0 @ U_tilde
+   U = Q @ U_tilde
    Sigma[:m,:m] = np.diag(S)
    return U, Sigma, V
 
 
 
+
 A=np.random.randn(100,200)
 l=5
+q=5
 U, S, V = randomizedSVD(A,l)
 A_reconstructed = U @ S @ V
 print(np.linalg.norm(A-A_reconstructed,ord='fro'))
-U, S, V = randomizedSVD_v2(A,l,1000)
+U, S, V = randomizedSVD_v2(A,l,q)
+A_reconstructed = U @ S @ V
+print(np.linalg.norm(A-A_reconstructed,ord='fro'))
+U, S, V = randomizedSVD_v3(A,l,q)
 A_reconstructed = U @ S @ V
 print(np.linalg.norm(A-A_reconstructed,ord='fro'))
