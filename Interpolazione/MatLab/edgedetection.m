402ad04e249b9dffb66931693dc911e32a2d26c5
Fonty02 <fontanaemanuele14@gmail.com>
2024-05-16 12:06:47 +0200

    Face recognition

--- Interpolazione/MatLab/edgedetection.m
+++ Interpolazione/MatLab/edgedetection.m
@@ -1,42 +1,42 @@
-A=imread('cameraman.tif');
-%imshow(A);
+A = imread('cameraman.tif');
+sz = size(A);
+xg = 1:sz(1);
+yg = 1:sz(2);
+F = griddedInterpolant({xg,yg},double(A));
 
-sz=size(A);
-xg=1:sz(1);
-yg=1:sz(2);
-F=griddedInterpolant({xg,yg},double(A));
-h=0.1;
+% Scelgo un incremento h:
+ h = 0.1;
 
-%Calcolo del gradiente
-grad_1=(F({xg+h,yg})-2.*F({xg,yg})+F({xg,yg+h}))./h;
-grad_2=(2.*F({xg,yg})-F({xg-h,yg})-F({xg,yg-h}))./h;
-grad=[grad_1(1:end-1,:);grad_2(end,:)]; %tutte le righe tranne ultima in avanti, ultima indietro
-grad=[grad(:,1:end-1) grad_2(:,end)]; %tutte le colonne tranne ultima in avanti, ultima indietro
+% Faccio sia le differenze in avanti che le differenze all'indietro 
+% per l'approssimazione del gradiente 
+grad_1 = (F({xg+h,yg}) - 2.*F({xg,yg}) + F({xg,yg+h}))./h;
+grad_2 = (2.*F({xg,yg})-F({xg-h,yg})- F({xg,yg-h}))./h;
 
-%imshow(uint8(grad));
+% Usiamo le differenze all'indietro per l'ultima riga e per l'ultima
+% colonna:
+grad = [grad_1(1:end-1,:);grad_2(end,:)];
+grad = [grad(:,1:end-1),grad_2(:,end)];
 
-[counts,x]=imhist(uint8(grad));
-T=otsuthresh(counts);
-BW=imbinarize(grad,T);
-%imshow(BW);
+figure()
+imshow(uint8(grad))
+title('Immagine gradiente')
+
+%L'immagine gradiente ci sonsente di evidenziare i bordi.
+
+%Per una migliore visualizzazione si usa un algoritmo di binarizzazione:
+% Scelta una certa soglia (o più soglie) si genera un'immagine in bianco e
+% nero. 
+
+%Scaliamo l'immagine in [0-1]
+
+Scaled = (grad - min(min(grad)))./(max(max(grad))-min(min(grad)));
+
+Temp = Scaled;
+Temp(Temp>=0.6) = 1;
+Temp(Temp<0.6) = 0;
+figure()
+imshow(Temp)
+title('Edge detection')
 
 
-%CREO CON SVD
-A=double(A);
-[U,S,V]=svd(A);
 
-n=50;
-Rec_100=U(:,1:n)*S(1:n,1:n)*V(:,1:n)';
-figure();
-imshow(uint8(Rec_100));
-
-Diff_i=abs(A-Rec_100);
-F=griddedInterpolant({xg,yg},Diff_i);
-grad_1=(F({xg+h,yg})-2.*F({xg,yg})+F({xg,yg+h}))./h;
-grad_2=(2.*F({xg,yg})-F({xg-h,yg})-F({xg,yg-h}))./h;
-grad=[grad_1(1:end-1,:);grad_2(end,:)]; %tutte le righe tranne ultima in avanti, ultima indietro
-grad=[grad(:,1:end-1) grad_2(:,end)]; %tutte le colonne tranne ultima in avanti, ultima indietro
-imshow(uint8(grad));
-imshow(uint8(Diff_i));
-
-Scaled=(Diff_i-min(min(Diff_i)))./(max(max(Diff_i))-min(min(Diff_i)));
---
+++ SVD/MatLab/svdDecompA.m
@@ -0,0 +1,88 @@
+function [A, U, fbar,M,ss1,ss2,N,RangeA] =svdDecompA
+
+
+%OUTPUT:
+% A matrice del nuovo gruppo di immagini 
+% U matrice di vettori ortogonali che spannano R(A)
+% fbar = immagine 'media'
+% M = numero totale di pixels
+% [ss1, ss2] = [righe,colonne] di ogni immagine
+% N = numero totale di immagini presenti nel database
+% RangeA = range di A
+
+%percorso di locazione del trial set---aggiornare con la propria
+%destinazione!!
+myFolder = 'C:\Users\fonta\Desktop\MetodiNumerici\SVD\MatLab\faces94\faces94\female\9336923';
+
+filePattern = fullfile(myFolder, '*.jpg');%cerco tutti e soli i files con estensione .jpg
+theFiles = dir(filePattern);% lista i files nella current folder
+N = length(theFiles);% numero totale di files presenti nella current folder
+
+
+for i=1:N
+    baseFileName = theFiles(i).name; %legge il nome del file in ordine lessicografico
+    fullFileName = fullfile(myFolder, baseFileName);% costruisce il path al file contenuto in baseFileName
+    fprintf('Now reading %s\n', fullFileName);
+    
+    % Trasformare l'immagine in '2D' passando alla scala di grigio per avere una MATRICE invece di un TENSORE
+    fi = rgb2gray(imread(fullFileName));
+    
+    %Voglio visualizzare nella stessa finestra tutte le immagini presenti
+    %nel database:
+    subplot(ceil(sqrt(N)),ceil(sqrt(N)),i);
+    figure(1); imshow(fi);
+    
+    %numero di righe e colonne di fi
+    [ss1,ss2] = size(fi); %ss1=righe, ss2=colonne, 
+    
+    M = ss1*ss2; % DIMENSIONE
+
+    S=zeros(M,N); %inizializzazione matrice delle facce
+    
+    fi = double(reshape(fi,M,1)); % ogni faccia diventa un vettore colonna
+    
+    S(:,i) = fi;%creazione matrice delle facce
+  
+end
+
+fbar = mean(S,2); %immagine media
+
+A = S-fbar; %matrice A -> ogni immagine sottrata la media
+
+[U, ~, ~] = svd(A, 0); %siccome mi serve solo la matrice U per il range di A, non mi interessa calcolare SVD completa. 
+rank(A)
+RangeA = U(:,rank(A)); %Range di A
+X = RangeA'*A;
+
+%prendo una faccia della stessa persona (ovvero una colonna di S)
+faccia_nota = S(:,1); %prima faccia del database
+%calcolo la proiezione della faccia nota sul range di A
+proiezione = RangeA'*(faccia_nota-fbar);
+size(proiezione)
+%calcolo la distanza tra la proiezione e ogni colonna di X e prendo la più piccola
+%distanza_min=valore massimo possibile
+distanza_min = inf;
+for i=1:N
+    distanza = norm(proiezione-X(:,i));
+    if distanza<distanza_min;
+        distanza_min = distanza;
+    end
+end
+distanza_min
+
+
+%prendo una faccia di una persona DIVERSA
+path_new_face = 'C:\Users\fonta\Desktop\MetodiNumerici\SVD\MatLab\faces94\faces94\female\anpage\anpage.1.jpg';
+new_face = double(rgb2gray(imread(path_new_face)));
+new_face = reshape(new_face,M,1);
+proiezione_new_face = RangeA'*(new_face-fbar);
+distanza_min_new_face = inf;
+for i=1:N
+    distanza = norm(proiezione_new_face-X(:,i));
+    if distanza<distanza_min_new_face;
+        distanza_min_new_face = distanza;
+    end
+end
+distanza_min_new_face
+
+
